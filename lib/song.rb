736e71113c37000465cbf1c35156669ca8bc8fd7
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-07 18:32:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,12 +7,8 @@ class Song
   end
 
 	def artist_name=(name)
-	  if (self.artist.nil?)
-		  self.artist = Artist.find_or_create_by_name(name)
-
-	  else
-		  self.artist.name = name
-	  end
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 
 	def self.new_by_filename(filename)
