082af1fa266e4f22adcd6db216282cd163424b1c
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-07 18:27:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,19 +6,19 @@ class Song
     @name = name
   end
 
-	def artist_name=(name) 
-	  if (self.artist.nil?)  
-		  self.artist = Artist.find_or_create_by_name(name) 
+	def artist_name=(name)
+	  if (self.artist.nil?)
+		  self.artist = Artist.find_or_create_by_name(name)
 
-	  else 
-		  self.artist.name = name 
-	  end  
-  end 
+	  else
+		  self.artist.name = name
+	  end
+  end
 
-	def self.new_by_filename(filename) 
-	  song_name = filename.split(" - ")[1] 
-	  new_song = self.new(song_name) 
-	  new_song.artist_name = filename.split(" - ")[0] 
-  	new_song 
-  end 
+	def self.new_by_filename(filename)
+	  song_name = filename.split(" - ")[1]
+	  new_song = self.new(song_name)
+	  new_song.artist_name = filename.split(" - ")[0]
+  	new_song
+  end
 end
